564331c94fd19dc54740d2239f9cf22271c547bf
Victor Maricato <victormaricato@cloud.upwork.com>
2024-10-24 00:28:42 +0200

    Revert "Summarize results by default"
    This reverts commit 86c61d56ecf4f7fae84e70713e5fca5426488431.

--- fastchat/serve/gradio_web_server.py
+++ fastchat/serve/gradio_web_server.py
@@ -1196,7 +1196,7 @@ def build_single_model_ui(demo, models, add_promotion_links=False, add_load_demo
             label="Max output tokens",
         )
         generate_thoughts = gr.Checkbox(value=True, label="Generate thoughts")
-        summarize_results = gr.Checkbox(value=True, label="Summarize results")
+        summarize_results = gr.Checkbox(value=False, label="Summarize results")
         text2cypher_prompt = gr.Textbox(
             label="Text2Cypher Prompt",
             lines=20,
